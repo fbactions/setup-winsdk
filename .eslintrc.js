fffebfc1b5ef1a22a04c791602eb20d49722f1c6
Frieder Bluemle <frieder.bluemle@gmail.com>
2020-06-07 01:25:14 -0700

    Switch ESLint config to @fbluemle

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     'es6': true,
     'node': true
   },
-  'extends': 'eslint:recommended',
+  'extends': '@fbluemle',
   'globals': {
     'Atomics': 'readonly',
     'SharedArrayBuffer': 'readonly'
