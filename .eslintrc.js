44267def6e7f7673aeab3abb5bb9e0014cd75a08
Frieder Bluemle <frieder.bluemle@gmail.com>
2021-10-28 01:19:12 -0700

    Integrate Prettier with ESLint config/plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,24 @@
 module.exports = {
-  'env': {
-    'commonjs': true,
-    'es6': true,
-    'node': true
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
   },
-  'extends': '@fbluemle',
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
+  extends: ['@fbluemle', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  'parserOptions': {
-    'ecmaVersion': 11
-  }
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  ignorePatterns: ['dist/'],
+  overrides: [
+    {
+      files: ['**/*.test.js'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
